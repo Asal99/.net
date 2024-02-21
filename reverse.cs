3e6fa0908f9d3d8329772b123696b81f56580385
Asal Poudel <112493561+Asal99@users.noreply.github.com>
2024-02-21 15:44:07 +0545

    Add files via upload

---
+++ reverse.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ncit_lab_asal1
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string:");
+            string? input = Console.ReadLine();
+
+            if (input != null)
+            {
+                // Print individual characters in reverse order
+                Console.WriteLine("Individual characters in reverse order:");
+                for (int i = input.Length - 1; i >= 0; i--)
+                {
+                    Console.WriteLine(input[i]);
+                }
+
+                // Display occurrence of individual characters
+                Dictionary<char, int> charOccurrences = new Dictionary<char, int>();
+                foreach (char c in input)
+                {
+                    if (charOccurrences.ContainsKey(c))
+                        charOccurrences[c]++;
+                    else
+                        charOccurrences[c] = 1;
+                }
+
+                Console.WriteLine("Character Occurrences:");
+                foreach (var kvp in charOccurrences)
+                {
+                    Console.WriteLine("'" + kvp.Key + "': " + kvp.Value + " times");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Input string is null.");
+            }
+        }
+    }
+}
